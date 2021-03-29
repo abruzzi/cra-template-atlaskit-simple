5c7da20f5d902fa86dced519a186abb780ecd3a6
Juntao Qiu <juntao.qiu@gmail.com>
2021-03-30 09:22:03 +1100

    fixed test with memory router

--- template/src/__tests__/app.tsx
+++ template/src/__tests__/app.tsx
@@ -1,8 +1,16 @@
 import { render } from "@testing-library/react";
 import App from "../app";
 
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+
 test("sign up form", async () => {
-  const { getByText } = render(<App />);
+  const history = createMemoryHistory()
+  const { getByText } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
   const element = getByText("Create a new account");
   expect(element).toBeDefined();
 });
