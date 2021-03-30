9883ab4d05f0cb1864cdf377fce471f7e0e0f794
Juntao Qiu <juntao.qiu@gmail.com>
2021-03-31 08:59:54 +1100

    clean up unit test a bit

--- template/src/__tests__/app.tsx
+++ template/src/__tests__/app.tsx
@@ -1,16 +1,18 @@
 import { render } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+
 import App from "../app";
 
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 
-test("sign up form", async () => {
-  const history = createMemoryHistory()
+test("Sign up form", () => {
+  const history = createMemoryHistory();
   const { getByText } = render(
     <Router history={history}>
       <App />
     </Router>
   );
   const element = getByText("Create a new account");
-  expect(element).toBeDefined();
+  expect(element).toBeInTheDocument();
 });
